1b79895589f11401ab72c8e71a5e93aacbc0ade9
SuryatejaParvata <96101598+SuryatejaParvata@users.noreply.github.com>
2022-06-06 19:03:25 +0530

    updated about page

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -20,36 +20,35 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I am Currently Learning MERN Stack at Masai School.
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Python. </b>
+                <b className="purple"> Html, Javascript and CSS. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                <b className="purple">Web Applications </b>
+                {" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
+                  
                 </b>
               </i>
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              with <b className="purple">Reactjs</b> 
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  
                 </b>
               </i>
-              &nbsp; like
+              &nbsp; 
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> </b>
               </i>
             </p>
           </Col>
